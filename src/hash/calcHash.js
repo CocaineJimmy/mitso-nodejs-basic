f6ef4d9806380ee2e4002d01718ae75ded78009c
CocaineJimmy <120776686+CocaineJimmy@users.noreply.github.com>
2025-04-24 16:44:33 +0300

    Hash (src/hash)

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,25 @@
 const calculateHash = async () => {
     // Write your code here 
+    const fs = require('fs');
+const path = require('path');
+const crypto = require('crypto');
+
+const filePath = path.join(__dirname, 'fileToCalculateHashFor.txt');
+
+// Если файла не существует, генерируем ошибку
+if (!fs.existsSync(filePath)) {
+  throw new Error('FS operation failed');
+}
+
+// Считываем данные файла
+const fileBuffer = fs.readFileSync(filePath);
+
+// Вычисляем SHA256 хэш
+const hash = crypto.createHash('sha256').update(fileBuffer).digest('hex');
+
+// Выводим хэш в консоль как hex строку
+console.log(hash);
+
 };
 
 await calculateHash();
